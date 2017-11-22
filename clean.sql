9399a199bee34dca28906e842ded2f2e35e9e072
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-22 21:27:52 +0000

    Started schema.sql and created auxiliary file clean.sql that drops all tables.

---
+++ clean.sql
@@ -0,0 +1,7 @@
+/* schema.sql: Ficheiro de criação do esquema da base de dados. */
+DROP TABLE produto;
+DROP TABLE constituida;
+DROP TABLE fornecedor;
+DROP TABLE categoria_simples;
+DROP TABLE super_categoria;
+DROP TABLE categoria;
--- schema.sql
+++ schema.sql
@@ -1,1 +1,43 @@
 /* schema.sql: Ficheiro de criação do esquema da base de dados. */
+/* schema.sql: Ficheiro de criação do esquema da base de dados. */
+CREATE TABLE categoria(
+	nome VARCHAR(200),
+	PRIMARY KEY (nome)
+);
+
+CREATE TABLE categoria_simples(
+	nome VARCHAR(200),
+	PRIMARY KEY (nome),
+	FOREIGN KEY (nome) REFERENCES categoria(nome)
+);
+
+CREATE TABLE super_categoria(
+	nome VARCHAR(200),
+	PRIMARY KEY (nome),
+	FOREIGN KEY (nome) REFERENCES categoria(nome)
+);
+
+CREATE TABLE constituida(
+	super_categoria VARCHAR(200),
+	categoria 		VARCHAR(200),
+	PRIMARY KEY (super_categoria, categoria),
+	FOREIGN KEY (categoria)	      REFERENCES categoria(nome),
+	FOREIGN KEY (super_categoria) REFERENCES super_categoria(nome)
+);
+
+CREATE TABLE fornecedor(
+	nif  NUMERIC(9),  -- NIF tem sempre 9 digitos
+	nome VARCHAR(200),
+	PRIMARY KEY (nif)
+);
+
+CREATE TABLE produto(
+	ean			  NUMERIC(13), -- EAN is always 13 digits
+	design		  VARCHAR(200),
+	categoria 	  VARCHAR(200),
+	forn_primario NUMERIC(9),
+	data		  DATE,
+	PRIMARY KEY (ean),
+	FOREIGN KEY (categoria)     REFERENCES categoria(nome),
+	FOREIGN KEY (forn_primario) REFERENCES fornecedor(nif)
+);
