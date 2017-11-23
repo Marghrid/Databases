4118fe7bfbdbadd8462024a8d45881b3b96edf27
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-23 20:37:06 +0000

    Added some categorias.

--- populate.sql
+++ populate.sql
@@ -1,1 +1,6 @@
 /* populate.sql: Ficheiro com instruções para popular as tabelas com dados de teste.  */
+
+INSERT INTO categoria VALUES ('lacticineos');
+INSERT INTO categoria VALUES ('produtos higienicos');
+INSERT INTO categoria VALUES ('fruta');
+INSERT INTO categoria VALUES ('comida de gato');
