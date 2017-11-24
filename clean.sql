9754178a840cef9c1eba60e631774f03d5bcf106
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-24 16:28:47 +0000

    Added some tables and restrictions.

--- clean.sql
+++ clean.sql
@@ -1,4 +1,10 @@
 /* schema.sql: Ficheiro de criação do esquema da base de dados. */
+DROP TABLE reposicao;
+DROP TABLE evento_reposicao;
+DROP TABLE planograma;
+DROP TABLE prateleira;
+DROP TABLE corredor;
+DROP TABLE fornece_sec;
 DROP TABLE produto;
 DROP TABLE constituida;
 DROP TABLE fornecedor;
--- schema.sql
+++ schema.sql
@@ -1,50 +1,98 @@
 /* schema.sql: Ficheiro de criação do esquema da base de dados. */
 /* schema.sql: Ficheiro de criação do esquema da base de dados. */
 CREATE TABLE categoria(
-	nome VARCHAR(200),
+	nome VARCHAR(200) NOT NULL,
 	PRIMARY KEY (nome)
 );
 
 CREATE TABLE categoria_simples(
-	nome VARCHAR(200),
+	nome VARCHAR(200) NOT NULL,
 	PRIMARY KEY (nome),
 	FOREIGN KEY (nome) REFERENCES categoria(nome)
 );
 
 CREATE TABLE super_categoria(
-	nome VARCHAR(200),
+	nome VARCHAR(200) NOT NULL,
 	PRIMARY KEY (nome),
 	FOREIGN KEY (nome) REFERENCES categoria(nome)
 );
 
 CREATE TABLE constituida(
-	super_categoria VARCHAR(200),
-	categoria 		VARCHAR(200),
+	super_categoria VARCHAR(200) 	NOT NULL,
+	categoria 		VARCHAR(200)	NOT NULL,
 	PRIMARY KEY (super_categoria, categoria),
-	FOREIGN KEY (categoria)	      REFERENCES categoria(nome),
-	FOREIGN KEY (super_categoria) REFERENCES super_categoria(nome)
+	FOREIGN KEY (categoria)	      	REFERENCES categoria(nome),
+	FOREIGN KEY (super_categoria)	REFERENCES super_categoria(nome),
+	CHECK (super_categoria != categoria)
 );
 
 CREATE TABLE fornecedor(
-	nif  NUMERIC(9),  -- NIF tem sempre 9 digitos
-	nome VARCHAR(200),
+	nif  NUMERIC(9)  	NOT NULL,  -- NIF tem sempre 9 digitos
+	nome VARCHAR(200)	NOT NULL,
 	PRIMARY KEY (nif)
 );
 
 CREATE TABLE produto(
-	ean			  NUMERIC(13), -- EAN is always 13 digits
-	design		  VARCHAR(200),
-	categoria 	  VARCHAR(200),
-	forn_primario NUMERIC(9),
-	data		  DATE,
+	ean			  NUMERIC(13) 	NOT NULL, -- EAN tem sempre 13 digitos
+	design		  VARCHAR(200)	NOT NULL,
+	categoria 	  VARCHAR(200)	NOT NULL,
+	forn_primario NUMERIC(9) 	NOT NULL,
+	data		  DATE 			NOT NULL,
 	PRIMARY KEY (ean),
 	FOREIGN KEY (categoria)     REFERENCES categoria(nome),
 	FOREIGN KEY (forn_primario) REFERENCES fornecedor(nif)
 );
 
 CREATE TABLE fornece_sec(
-	nif	NUMERIC(9),
-	ean NUMERIC(13),
+	nif	NUMERIC(9)  NOT NULL,
+	ean NUMERIC(13)	NOT NULL,
 	PRIMARY KEY (nif, ean),
-	FOREIGN 
+	FOREIGN KEY (ean) REFERENCES produto(ean)
+);
+
+CREATE TABLE corredor(
+	nro     SMALLINT	NOT NULL,  -- o supermercado tem menos de 32767 corredores
+	largura	SMALLINT	NOT NULL,  -- em centimetros. Corredor tem menos de 327 metros
+	PRIMARY KEY (nro)
+);
+
+CREATE TABLE prateleira(
+	nro    SMALLINT 	NOT NULL,
+	lado   VARCHAR(8)	NOT NULL,  -- "esquerdo" ou "direito"
+	altura VARCHAR(8) 	NOT NULL,  -- "chão", "médio" ou "superior"
+	PRIMARY KEY (nro, lado, altura),
+	FOREIGN KEY (nro) REFERENCES corredor
+);
+
+CREATE TABLE planograma(
+	ean      NUMERIC(13)	NOT NULL,
+	nro      SMALLINT  		NOT NULL,
+	lado     VARCHAR(8)		NOT NULL,
+	altura   VARCHAR(8) 	NOT NULL,
+	face     SMALLINT		NOT NULL, -- número de frentes de produto visíveis
+	unidades INT			NOT NULL,
+	loc      INT   	        NOT NULL, -- número de slot. O supermercado tem menos de 2147483647 slots.
+	PRIMARY KEY (ean, nro, lado, altura),
+	FOREIGN KEY	(nro, lado, altura)	REFERENCES prateleira(nro, lado, altura)
+);
+
+CREATE TABLE evento_reposicao(
+	operador	NUMERIC(9)	NOT NULL UNIQUE, -- nif do empregado
+	instante	TIMESTAMP	NOT NULL,
+	PRIMARY KEY (instante)
+	--UNIQUE (operador, instante)
+);
+
+CREATE TABLE reposicao(
+	ean 		NUMERIC(13)	NOT NULL,
+	nro 		SMALLINT	NOT NULL,
+	lado		VARCHAR(8)	NOT NULL,
+	altura 		VARCHAR(8)  NOT NULL,
+	operador 	NUMERIC(9)	NOT NULL,
+	instante 	TIMESTAMP	NOT NULL,
+	unidades 	INT 		NOT NULL,
+	PRIMARY KEY (ean, nro, lado, altura, operador, instante),
+	FOREIGN KEY (ean, nro, lado, altura)	REFERENCES	planograma(ean, nro, lado, altura),
+	FOREIGN KEY (operador)		REFERENCES  evento_reposicao(operador),
+	FOREIGN KEY (instante)		REFERENCES  evento_reposicao(instante)
 );
