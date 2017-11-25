8294e437be6d4a9a3a041a7d805275d204838851
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-25 11:28:57 +0000

    Upload forgotten file xD

---
+++ web/not_implemented.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    	<p>This functionality is not yet implemented.</p>
+    	<p><a href="supermercado.php">Voltar</a></p>
+
+    </body>
+</html>
