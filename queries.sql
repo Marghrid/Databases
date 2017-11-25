0de8bb607e863e153ded7039a1014966f11b7faf
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-25 18:03:41 +0000

    Populated and made 2 queries.

--- queries.sql
+++ queries.sql
@@ -7,12 +7,21 @@
 --    todas as categorias simples?
 
 -- c) Quais os produtos (ean) que nunca foram repostos?
+SELECT ean
+FROM produto
+WHERE ean NOT IN (
+	SELECT ean
+	FROM reposicao
+);
 
 -- d) Quais os produtos (ean) com um número de fornecedores secundários
 --    superior a 10?
+SELECT ean
+FROM fornece_sec
+GROUP BY ean
+HAVING COUNT(fornece_sec) > 10;
 
 -- e) Quais os produtos (ean) que foram repostos sempre pelo mesmo operador?
-
 SELECT ean
 FROM reposicao
 GROUP BY ean
