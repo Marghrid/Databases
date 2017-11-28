4f62fe8b13c363f993991356f7a3b1708cabf7d9
miguelmarques1904 <migsmarques@gmail.com>
2017-11-28 19:33:48 +0000

    voltar now works again

--- web/adicionar_forn_sec.php
+++ web/adicionar_forn_sec.php
@@ -46,7 +46,7 @@
             {
                 echo("<p>ERROR: {$e->getMessage()}</p>");
             }
-            echo("<p><a href=\"alterar_fornecedores.php?ean=$ean&design=$design&forn_prim=$forn_prim\"> Voltar</a></p>");
+            echo("<p><a href=\"ver_fornecedores.php?forn_prim=$forn_prim&ean=$ean&design=$design\"> Voltar</a></p>");
             echo("<p><a href=\"supermercado.php\">Ver supermercado</a></p>");
         ?>
     </body>
