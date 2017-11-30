32c5f7b5874a7cf0a6799bef6a808d669fda9e09
Margarida Ferreira <margaridaacferreira@tecnico.ulisboa.pt>
2017-11-30 19:28:12 +0000

    Moved clean to schema, because apparently, that's the norm.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,22 @@
 /* schema.sql: Ficheiro de criação do esquema da base de dados. */
 
+DROP TABLE IF EXISTS reposicao;
+DROP TABLE IF EXISTS evento_reposicao;
+DROP TABLE IF EXISTS planograma;
+DROP TABLE IF EXISTS prateleira;
+DROP TABLE IF EXISTS corredor;
+DROP TABLE IF EXISTS fornece_sec;
+DROP TABLE IF EXISTS produto;
+DROP TABLE IF EXISTS constituida;
+DROP TABLE IF EXISTS fornecedor;
+DROP TABLE IF EXISTS categoria_simples;
+DROP TABLE IF EXISTS super_categoria;
+DROP TABLE IF EXISTS categoria;
+
+DROP TYPE IF EXISTS tipo_lado;
+DROP TYPE IF EXISTS tipo_altura;
+
+
 CREATE TYPE tipo_lado   AS ENUM ('direito', 'esquerdo');
 CREATE TYPE tipo_altura AS ENUM ('chao', 'medio', 'superior');
 
